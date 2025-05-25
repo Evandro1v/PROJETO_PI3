34b68a89d0c1074c6e88a0f4a424a0e3308e9dbb
Evandro1v <evandro1vando@gmail.com>
2025-05-25 13:08:55 -0300

    colocando online

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const app = express(); // Cria uma instância do aplicativo Express.
 const admin = require('firebase-admin');
 
 // Configure Firebase Admin SDK com as credenciais da variável de ambiente
-// admin.initializeApp({
-//   credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK)),
-//   storageBucket: 'gs://sitetapburaco.appspot.com' // Substitua 'seu_bucket' pelo nome do seu bucket no Firebase Storage
-// });
 admin.initializeApp({
-  credential: admin.credential.cert('sitetapburaco-firebase-adminsdk-gnu38-c2134532bb.json'),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK)),
   storageBucket: 'gs://sitetapburaco.appspot.com' // Substitua 'seu_bucket' pelo nome do seu bucket no Firebase Storage
 });
+// admin.initializeApp({
+//   credential: admin.credential.cert('sitetapburaco-firebase-adminsdk-gnu38-c2134532bb.json'),
+//   storageBucket: 'gs://sitetapburaco.appspot.com' // Substitua 'seu_bucket' pelo nome do seu bucket no Firebase Storage
+// });
 
 
 app.engine('handlebars', engine()); // Configura o motor de visualização Handlebars no Express.
